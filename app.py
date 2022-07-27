551658e943bedbd08a9404a4d573ba425e794211
rosemeirepa <prosemeire@yahoo.com.br>
2022-07-27 00:32:33 +0000

    Criação da base html e style css

--- app.py
+++ app.py
@@ -1,12 +1,25 @@
 from flask import Flask, render_template
+from datetime import datetime
 
 app = Flask("hello")
 
+# Mock do banco de dados
+posts = [
+    {
+        "title": "O meu Primeiro Post",
+        "body": "Aqui é o texto do Post",
+        "author": "Rose",
+        "created": datetime(2022,7,25)
+    },
+    {
+        "title": "O meu Segundo Post",
+        "body": "Aqui é o texto do Post",
+        "author": "Meire",
+        "created": datetime(2022,7,26)
+    },
+]
+
 # Criação de rota para a aplicação - Comando route tb permite a criação de uma função (o @ é uma marcação para a rota)
 @app.route("/")
-def hello():
-    return "Hello Word"
-
-@app.route("/meucontato")
-def meuContato():
-    return  render_template('index.html')
+def index():
+    return render_template("index.html", posts=posts)
